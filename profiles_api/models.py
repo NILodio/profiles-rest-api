4303636b20f2a80d158e42eda3d9cc8a5f5ce83a
Danilo Diaz Valencia <ddiazva13@gmail.com>
2021-02-16 15:40:29 -0500

    Enable Django admin for user and super user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,25 +5,25 @@ from django.contrib.auth.models import BaseUserManager
 
 
 
-class UserProfileManager(BaseException):
+class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email,name,password=None):
-        """Create a new user profiles"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User must have a email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.models(email = email, name = name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, name, password):
-        """Create and save a new superuser with gicen details"""
-        user =self.create_user(user, name,password)
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -45,12 +45,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of user"""
+        """Retrieve full name for user"""
         return self.name
+
     def get_short_name(self):
-        """Retrive short name of user"""
+        """Retrieve short name of user"""
         return self.name
-    
+
     def __str__(self):
         """Return string representation of user"""
         return self.email

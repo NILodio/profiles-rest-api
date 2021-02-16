b41f24d8aaec1161b17feab1bc20a1980968822a
Danilo Diaz Valencia <ddiazva13@gmail.com>
2021-02-16 16:07:56 -0500

    .gitignore change

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self, request , format = None):
+        """Returns a list of APIView feactures"""
+        an_apiview = [
+            'Uses HTTP methods as fuctions (get, post, put, delete)',
+            'Is similar to a tradicional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message':'Hello','an_apiview':an_apiview})
